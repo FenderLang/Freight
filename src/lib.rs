b4a0348c34c6c04dbb055d13f2969e703fba9a92
Marcuse <julien.marcuse@mymail.champlain.edu>
2023-01-24 12:03:36 -0500

    Create Function and VMWriter

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,25 @@
-pub struct ExecutionContext<V> {
+use std::collections::HashMap;
+
+pub struct Function<V: Clone + Default> {
+    instructions: Vec<Instruction<V>>,
+    args: usize,
+}
+
+pub struct VMWriter<V: Clone + Default> {
+    functions: HashMap<String, Function<V>>,
+}
+
+impl<V: Clone + Default> VMWriter<V> {
+    fn write_function(&mut self, function: Function<V>) {
+        todo!()
+    }
+    
+    fn finish(self) -> ExecutionContext<V> {
+        todo!()
+    }
+}
+
+pub struct ExecutionContext<V: Clone + Default> {
     stack: Vec<V>,
     instructions: Vec<Instruction<V>>,
     instruction: usize,
@@ -6,6 +27,7 @@ pub struct ExecutionContext<V> {
     frame: usize,
     return_value: V,
 }
+
 
 impl<V: Clone + Default> ExecutionContext<V> {
     pub fn new(instructions: Vec<Instruction<V>>, stack_size: usize) -> ExecutionContext<V> {
@@ -19,19 +41,20 @@ impl<V: Clone + Default> ExecutionContext<V> {
         }
     }
 
-    fn get(&self, offset: &usize) -> &V {
+    fn get(&self, offset: usize) -> &V {
         &self.stack[self.frame + offset]
     }
 
-    fn get_mut(&mut self, offset: &usize) -> &mut V {
+    fn get_mut(&mut self, offset: usize) -> &mut V {
         &mut self.stack[self.frame + offset]
     }
 
-    fn execute(&mut self, instruction: &Instruction<V>) {
+    fn execute(&mut self, index: usize) {
+        let instruction = &self.instructions[index];
         match instruction {
-            Instruction::Create(offset, creator) => *self.get_mut(offset) = creator(self),
-            Instruction::Move(from, to) => *self.get_mut(to) = self.get(from).clone(),
-            Instruction::MoveReturn(to) => *self.get_mut(to) = std::mem::replace(&mut self.return_value, V::default()),
+            Instruction::Create(offset, creator) => *self.get_mut(*offset) = creator(self),
+            Instruction::Move(from, to) => *self.get_mut(*to) = self.get(*from).clone(),
+            Instruction::MoveReturn(to) => *self.get_mut(*to) = std::mem::replace(&mut self.return_value, V::default()),
             Instruction::Invoke(args, stack_size, instruction) => {
                 self.frames.push(self.frame);
                 self.frame -= args;
@@ -42,7 +65,7 @@ impl<V: Clone + Default> ExecutionContext<V> {
             }
             Instruction::InvokeNative(func) => self.return_value = func(self),
             Instruction::Return(offset) => {
-                self.return_value = self.get(offset).clone();
+                self.return_value = self.get(*offset).clone();
                 self.frame = self.frames.pop().unwrap();
             }
             Instruction::ReturnConstant(c) => {
@@ -51,9 +74,16 @@ impl<V: Clone + Default> ExecutionContext<V> {
             }
         }
     }
+    
+    fn run(&mut self) {
+        while self.instruction < self.instructions.len() {
+            self.execute(self.instruction);
+            self.instruction += 1;
+        }
+    }
 }
 
-pub enum Instruction<V> {
+pub enum Instruction<V: Default + Clone> {
     Create(usize, fn(&ExecutionContext<V>) -> V),
     Move(usize, usize),
     MoveReturn(usize),
@@ -61,9 +91,4 @@ pub enum Instruction<V> {
     InvokeNative(fn(&mut ExecutionContext<V>) -> V),
     Return(usize),
     ReturnConstant(V),
-}
-
-#[derive(Clone)]
-pub enum Value {
-    Null,
 }
